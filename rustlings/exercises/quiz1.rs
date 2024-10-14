a57dfd83f47519e7d3372b32f324ffe3dbe34055
Artem Trokhymchuk <trkhmchk@gmail.com>
2024-10-15 00:51:48 +0300

    add 1-22

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -8,10 +8,15 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
+// I AM DONE
 
 // Put your function here!
-// fn calculate_apple_price {
+ fn calculate_apple_price(n: u32) -> u32 {
+    if n > 40 {
+        return n;
+    }
+    n * 2
+ }
 
 // Don't modify this function!
 #[test]
